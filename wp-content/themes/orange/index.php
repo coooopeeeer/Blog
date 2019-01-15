335beadf2738d4920fbfaf6c2eb0d535c8ea91ec
coooopeeeer <miminashicooper@gmail.com>
2019-01-15 16:05:32 +0900

    Make layout for columns

--- wp-content/themes/orange/index.php
+++ wp-content/themes/orange/index.php
@@ -1,23 +1,29 @@
 <?php get_header(); ?>
 
-<main>
-    <?php
-        if ( have_posts() ) : while ( have_posts() ) : the_post();
-    ?>
-    <section class="post">
-        <h2>
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-        </h2>
-        <?php the_excerpt(); ?>
-    </section>
-    <?php
-        endwhile;
-        endif;
-    ?>
-</main>
+<div id="container">
+    <div class="wrapper">
+        <div class="content">
+            <main>
+                <?php
+                    if ( have_posts() ) : while ( have_posts() ) : the_post();
+                ?>
+                <section class="post">
+                    <h2>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+                    <?php the_excerpt(); ?>
+                </section>
+                <?php
+                    endwhile;
+                    endif;
+                ?>
+            </main>
 
-<?php get_sidebar(); ?>
+            <?php get_sidebar(); ?>
+        </div><!-- End div.content -->
+    </div><!-- End div.wrapper -->
+</div><!-- End div#container -->
 
 <?php get_footer(); ?>
