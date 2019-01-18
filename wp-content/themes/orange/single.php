75ba20dea6a6df74b6fb484e6e5cf8b3afcc6a93
coooopeeeer <miminashicooper@gmail.com>
2019-01-19 00:59:40 +0900

    Modify styles on detail pages

--- wp-content/themes/orange/single.php
+++ wp-content/themes/orange/single.php
@@ -1,21 +1,27 @@
 <?php get_header(); ?>
 
-<main>
-    <?php
-        if ( have_posts() ) : while ( have_posts() ) : the_post();
-    ?>
-    <article class="content">
-        <h2>
-            <?php the_title(); ?>
-        </h2>
-        <?php the_content(); ?>
-    </article>
-    <?php
-        endwhile;
-        endif;
-    ?>
-</main>
+<div id="container">
+    <div class="wrapper">
+        <div class="content">
+            <main>
+                <?php
+                    if ( have_posts() ) : while ( have_posts() ) : the_post();
+                ?>
+                <article>
+                    <h1>
+                        <?php the_title(); ?>
+                    </h1>
+                    <?php the_content(); ?>
+                </article>
+                <?php
+                    endwhile;
+                    endif;
+                ?>
+            </main>
 
-<?php get_sidebar(); ?>
+            <?php get_sidebar(); ?>
+        </div><!-- End div.content -->
+    </div><!-- End div.wrapper -->
+</div><!-- End div#container -->
 
 <?php get_footer(); ?>
